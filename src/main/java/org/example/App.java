2fca1dbdcd5a4feca6b8fe9696660220946db76a
sokolovstanislav <ignateva-1946@mail.ru>
2023-07-21 10:41:09 +0300

    Lesson52. Added item.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,16 @@
 package org.example;
 
 
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 
 /**
  * Hello world!
@@ -16,18 +20,33 @@ public class App
 {
     public static void main( String[] args ) {
 
-        SessionFactory sessionFactory = new Configuration().addAnnotatedClass(Person.class).buildSessionFactory();
+        SessionFactory sessionFactory = new Configuration().addAnnotatedClass(Person.class).
+                addAnnotatedClass(Item.class).buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
+
+
+            Person person = session.get(Person.class, 2010);
+            Item item = new Item("Brothers Karamazovs", person);
+
+            person.getItems().add(item);
+
+            session.save(item);
+
+
+//            Person person = new Person("Name", 30, "email@mail.ru", "Kudrovo");
+//            Item item = new Item("Test cascading item", person);
+//            person.setItems(new ArrayList<>(Collections.singletonList(item)));
 
 //            List<Person> people = session.createQuery("FROM Person WHERE name LIKE 'T%'").getResultList();
 //            session.createQuery("update Person set name = 'Test' WHERE age > 30").executeUpdate();
 //            for (Person p : people) {
 //                System.out.println(p.getName());
 //            }
-            session.createQuery("delete from Person where age > 30").executeUpdate();
+//            session.createQuery("delete from Person where age > 30").executeUpdate();
+
 
             session.getTransaction().commit();
         } finally {
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -3,6 +3,8 @@ package org.example.model;
 import jdk.nashorn.internal.objects.annotations.Constructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "person")
@@ -20,6 +22,9 @@ public class Person {
     private String email;
     @Column(name = "address")
     private String address;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Item> items;
 
     public Person(String name, int age, String email, String address) {
         this.name = name;
@@ -70,4 +75,30 @@ public class Person {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public void setItems(ArrayList<Item> items) {
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", items=" + items +
+                '}';
+    }
+//    public void addItem(Item item) {
+//        if (this.items == null) this.items = new ArrayList<>();
+//
+//        this.items.add(item);
+//        item.setOwner(this);
+//    }
+
 }
